66e6dcba7dc06c7b2a5aec485465d1eee0638470
andres.sanchez <andres.sanchez@fluxit.com.ar>
2020-07-27 18:11:07 -0300

    Update de productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbListModule, NbCardModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { ProductosComponent } from './productos/productos.component';
 import { HomeComponent } from './home/home.component';
@@ -21,7 +21,9 @@ import { HomeComponent } from './home/home.component';
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'dark' }),
     NbLayoutModule,
-    NbEvaIconsModule
+    NbEvaIconsModule,
+    NbListModule,
+    NbCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductosComponent implements OnInit {
 
+  public products: any[] = [
+    { name: 'Rysen 5', id: 1 },
+    { name: 'Rysen 5', id: 2 },
+    { name: 'Rysen 7', id: 3 },
+    { name: 'Rysen 7', id: 4 },
+    { name: 'Intel i3', id: 5 },
+    { name: 'Intel i3', id: 6 },
+    { name: 'Intel i5', id: 7 },
+    { name: 'Intel i5', id: 8 },
+    { name: 'Intel i7', id: 9 },
+    { name: 'Intel i7', id: 10 }
+  ];
+
+  public categories: any[] = [
+    { name: 'Procesadores', id: 1 },
+    { name: 'Motherboards', id: 2 },
+    { name: 'Memorias RAM', id: 3 },
+    { name: 'Placa de video', id: 4 },
+    { name: 'Almacenamiento', id: 5 },
+    { name: 'Gabinete', id: 6 },
+    { name: 'Fuentes', id: 7 },
+    { name: 'Refrigeración', id: 8 },
+    { name: 'Conectividad', id: 9 },
+    { name: 'Lectoras', id: 10 }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+
+
+
+
